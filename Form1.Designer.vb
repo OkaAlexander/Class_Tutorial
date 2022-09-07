6664983b4960c85a3d6ee00693ec6f08491ea4ed
Alexander OKA <oka.learn.technology@gmail.com>
2022-09-07 02:58:15 +0000

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,327 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtpid = New System.Windows.Forms.TextBox()
+        Me.txtproductsellprice = New System.Windows.Forms.TextBox()
+        Me.txtbuyprice = New System.Windows.Forms.TextBox()
+        Me.txtpname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtdatetimepicker = New System.Windows.Forms.DateTimePicker()
+        Me.radiolocalproduct = New System.Windows.Forms.RadioButton()
+        Me.radioforeignproduct = New System.Windows.Forms.RadioButton()
+        Me.cmdwarriantytype = New System.Windows.Forms.ComboBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnupdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtsearch = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(157, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(423, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CRUD APPLICATION FOR BEGINNERS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(181, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Product ID:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(181, 337)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Expd Date:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(181, 288)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(99, 16)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Selling Price:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(181, 239)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(98, 16)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Buying Price:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(181, 187)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(109, 16)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Product Name:"
+        '
+        'txtpid
+        '
+        Me.txtpid.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpid.Location = New System.Drawing.Point(338, 135)
+        Me.txtpid.Multiline = True
+        Me.txtpid.Name = "txtpid"
+        Me.txtpid.Size = New System.Drawing.Size(296, 26)
+        Me.txtpid.TabIndex = 8
+        '
+        'txtproductsellprice
+        '
+        Me.txtproductsellprice.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtproductsellprice.Location = New System.Drawing.Point(338, 278)
+        Me.txtproductsellprice.Multiline = True
+        Me.txtproductsellprice.Name = "txtproductsellprice"
+        Me.txtproductsellprice.Size = New System.Drawing.Size(296, 26)
+        Me.txtproductsellprice.TabIndex = 9
+        '
+        'txtbuyprice
+        '
+        Me.txtbuyprice.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbuyprice.Location = New System.Drawing.Point(338, 229)
+        Me.txtbuyprice.Multiline = True
+        Me.txtbuyprice.Name = "txtbuyprice"
+        Me.txtbuyprice.Size = New System.Drawing.Size(296, 26)
+        Me.txtbuyprice.TabIndex = 10
+        '
+        'txtpname
+        '
+        Me.txtpname.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpname.Location = New System.Drawing.Point(338, 177)
+        Me.txtpname.Multiline = True
+        Me.txtpname.Name = "txtpname"
+        Me.txtpname.Size = New System.Drawing.Size(296, 26)
+        Me.txtpname.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(181, 380)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 16)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Warrianty Type:"
+        '
+        'txtdatetimepicker
+        '
+        Me.txtdatetimepicker.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtdatetimepicker.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.txtdatetimepicker.Location = New System.Drawing.Point(337, 329)
+        Me.txtdatetimepicker.Name = "txtdatetimepicker"
+        Me.txtdatetimepicker.Size = New System.Drawing.Size(286, 24)
+        Me.txtdatetimepicker.TabIndex = 13
+        '
+        'radiolocalproduct
+        '
+        Me.radiolocalproduct.AutoSize = True
+        Me.radiolocalproduct.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radiolocalproduct.Location = New System.Drawing.Point(337, 438)
+        Me.radiolocalproduct.Name = "radiolocalproduct"
+        Me.radiolocalproduct.Size = New System.Drawing.Size(118, 22)
+        Me.radiolocalproduct.TabIndex = 14
+        Me.radiolocalproduct.TabStop = True
+        Me.radiolocalproduct.Text = "Local Product"
+        Me.radiolocalproduct.UseVisualStyleBackColor = True
+        '
+        'radioforeignproduct
+        '
+        Me.radioforeignproduct.AutoSize = True
+        Me.radioforeignproduct.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radioforeignproduct.Location = New System.Drawing.Point(503, 438)
+        Me.radioforeignproduct.Name = "radioforeignproduct"
+        Me.radioforeignproduct.Size = New System.Drawing.Size(132, 22)
+        Me.radioforeignproduct.TabIndex = 15
+        Me.radioforeignproduct.TabStop = True
+        Me.radioforeignproduct.Text = "Foreign Product"
+        Me.radioforeignproduct.UseVisualStyleBackColor = True
+        '
+        'cmdwarriantytype
+        '
+        Me.cmdwarriantytype.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdwarriantytype.FormattingEnabled = True
+        Me.cmdwarriantytype.Items.AddRange(New Object() {"Express Warrianty", "Extended warrianty", "Honour warrianty"})
+        Me.cmdwarriantytype.Location = New System.Drawing.Point(337, 370)
+        Me.cmdwarriantytype.Name = "cmdwarriantytype"
+        Me.cmdwarriantytype.Size = New System.Drawing.Size(286, 26)
+        Me.cmdwarriantytype.TabIndex = 16
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.Green
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(259, 520)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(126, 51)
+        Me.btnAdd.TabIndex = 17
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnupdate
+        '
+        Me.btnupdate.BackColor = System.Drawing.Color.Yellow
+        Me.btnupdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnupdate.Location = New System.Drawing.Point(528, 520)
+        Me.btnupdate.Name = "btnupdate"
+        Me.btnupdate.Size = New System.Drawing.Size(126, 51)
+        Me.btnupdate.TabIndex = 18
+        Me.btnupdate.Text = "Update"
+        Me.btnupdate.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(392, 520)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(126, 51)
+        Me.btnDelete.TabIndex = 19
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(696, 75)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(923, 530)
+        Me.DataGridView1.TabIndex = 20
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(1198, 18)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(126, 36)
+        Me.btnSearch.TabIndex = 21
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtsearch
+        '
+        Me.txtsearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsearch.Location = New System.Drawing.Point(885, 22)
+        Me.txtsearch.Multiline = True
+        Me.txtsearch.Name = "txtsearch"
+        Me.txtsearch.Size = New System.Drawing.Size(296, 32)
+        Me.txtsearch.TabIndex = 22
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(181, 438)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(104, 16)
+        Me.Label4.TabIndex = 23
+        Me.Label4.Text = "Product Type:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1649, 727)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtsearch)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnupdate)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.cmdwarriantytype)
+        Me.Controls.Add(Me.radioforeignproduct)
+        Me.Controls.Add(Me.radiolocalproduct)
+        Me.Controls.Add(Me.txtdatetimepicker)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtpname)
+        Me.Controls.Add(Me.txtbuyprice)
+        Me.Controls.Add(Me.txtproductsellprice)
+        Me.Controls.Add(Me.txtpid)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtpid As TextBox
+    Friend WithEvents txtproductsellprice As TextBox
+    Friend WithEvents txtbuyprice As TextBox
+    Friend WithEvents txtpname As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtdatetimepicker As DateTimePicker
+    Friend WithEvents radiolocalproduct As RadioButton
+    Friend WithEvents radioforeignproduct As RadioButton
+    Friend WithEvents cmdwarriantytype As ComboBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnupdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtsearch As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    'connecting your app to the database'
+    Dim con As New SqlConnection("Data Source=OKAH;Initial Catalog=mrsellerDB;Integrated Security=True")
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        'declare variables that will take values from the various form control'
+        Dim pId As Integer = txtpid.Text
+        Dim pNmae As String = txtpname.Text
+        Dim buyPrice As Double = txtbuyprice.Text
+        Dim sellPrice As Double = txtproductsellprice.Text
+        Dim pexpireDate As DateTime = txtdatetimepicker.Text
+        Dim warriantyType As String = cmdwarriantytype.Text
+        Dim productType As String = ""
+        If radioforeignproduct.Checked = True Then
+            productType = "Foreign product"
+        Else
+            productType = "Local product"
+
+        End If
+
+        'you must open the db first before u insert, close it after insert'
+        'sql command to add data to your product taype'
+        Try
+
+            con.Open()
+            Dim command As New SqlCommand("insert into ProductT values('" & pId & "','" & pNmae & "','" & buyPrice & "','" & sellPrice & "','" & pexpireDate & "','" & warriantyType & "','" & productType & "')", con)
+            'now let execute our command'
+            command.ExecuteNonQuery()
+            con.Close()
+            LoadDataInGrid()
+            MessageBox.Show("Product add successfully..")
+            txtpid.Clear()
+            txtpname.Clear()
+            txtbuyprice.Clear()
+        Catch ex As Exception
+            Throw New Exception(ex.Message & " Not again!")
+
+        End Try
+    End Sub
+    'let display our data in a grid by creating a sub class'
+    Private Sub LoadDataInGrid()
+        Dim command As New SqlCommand("select * from ProductT", con)
+        'Dim command As New SqlCommand("select * from ProductT")
+        Dim sda As New SqlDataAdapter(Command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        DataGridView1.DataSource = dt
+        'now call your class which is LoadDataInGrid() under the form load and also under the add botton event'
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataInGrid()
+    End Sub
+
+    Private Sub btnupdate_Click(sender As Object, e As EventArgs) Handles btnupdate.Click
+        Dim pId As Integer = txtpid.Text
+        Dim pNmae As String = txtpname.Text
+        Dim buyPrice As Double = txtbuyprice.Text
+        Dim sellPrice As Double = txtproductsellprice.Text
+        Dim pexpireDate As DateTime = txtdatetimepicker.Text
+        Dim warriantyType As String = cmdwarriantytype.Text
+        Dim productType As String = ""
+        If radioforeignproduct.Checked = True Then
+            productType = "Foreign product"
+        Else
+            productType = "Local product"
+
+        End If
+
+        Try
+
+            con.Open()
+            Dim command As New SqlCommand("update ProductT  set productID='" & pId & "',productName='" & pNmae & "', buyPrice='" & buyPrice & "', sellPrice='" & sellPrice & "', productExpiredate='" & pexpireDate & "',warratyType='" & warriantyType & "', productType='" & productType & "' where productID='" & pId & "'", con)
+            'now let execute our command'
+            command.ExecuteNonQuery()
+            con.Close()
+            LoadDataInGrid()
+            MessageBox.Show("Product updated..")
+        Catch ex As Exception
+            Throw New Exception(ex.Message & " Not again!")
+
+        End Try
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If MessageBox.Show("are u sure u want to delete the selected record", "warning", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+
+            Dim pId As Integer = txtpid.Text
+            con.Open()
+            Dim command As New SqlCommand("Delete ProductT  where productID='" & pId & "'", con)
+            'now let execute our command'
+            command.ExecuteNonQuery()
+            MessageBox.Show("record deleted succefully")
+            con.Close()
+            LoadDataInGrid()
+        End If
+
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim pId As Integer = txtsearch.Text
+        Dim command As New SqlCommand("select * from ProductT where productID='" & pId & "'", con)
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+
+End Class
